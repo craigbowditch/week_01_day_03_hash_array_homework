83cac8116f6541fb2de0d5c3ede9ed4d0768fa44
Craig Bowditch <craigbowditch@hotmail.com>
2018-07-18 20:55:17 +0100

    completes third test exercise c

--- homework_exercise_c.rb
+++ homework_exercise_c.rb
@@ -21,3 +21,8 @@ united_kingdom[1][:capital] = "Cardiff"
 
 #task 2
 united_kingdom[3] = {name: "Northern Ireland", population: 1811000, capital: "Belfast"}
+
+#task 3
+for countries in united_kingdom
+  p "#{countries[:name]}"
+end
