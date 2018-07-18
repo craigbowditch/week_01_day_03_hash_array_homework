b5a68c8d282db862b404d1ae8604e2b51f70abea
Craig Bowditch <craigbowditch@hotmail.com>
2018-07-18 20:35:49 +0100

    completes first task exercise c

---
+++ homework_exercise_c.rb
@@ -0,0 +1,20 @@
+united_kingdom = [
+  {
+    name: "Scotland",
+    population: 5295000,
+    capital: "Edinburgh"
+  },
+  {
+    name: "Wales",
+    population: 3063000,
+    capital: "Swansea"
+  },
+  {
+    name: "England",
+    population: 53010000,
+    capital: "London"
+  }
+]
+
+#task 1
+united_kingdom[1][:capital] = "Cardiff"
